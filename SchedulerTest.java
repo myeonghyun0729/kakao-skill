5e6e6584363c6636835136be4e55472cdbcb014c
myeonghyun <96726016+myeonghyun0729@users.noreply.github.com>
2025-08-01 10:12:01 +0900

    Create SchedulerTest.java

---
+++ SchedulerTest.java
@@ -0,0 +1,43 @@
+package com.example.demo.config;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Component
+public class SchedulerTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(SchedulerTest.class);
+
+    private final String PROFILE_PROD_ONE = "prodOne";
+
+    private final String PROFILE_PROD_TWO = "prodTwo";
+
+
+    private static String activeProfile;
+
+    @PostConstruct
+    public void init() {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            activeProfile = ip.getHostAddress().equals("xxx.xxx.xxx") ? PROFILE_PROD_ONE : PROFILE_PROD_TWO;
+        } catch (UnknownHostException e) {
+            logger.info("ip check failed : {}", e.getMessage());
+        }
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void scheduledTask() {
+        // active profile check
+        if (activeProfile.equals(PROFILE_PROD_ONE)) {
+            logger.info("scheduler stop!");
+            return;
+        }
+        logger.info("scheduler run!");
+    }
+}
